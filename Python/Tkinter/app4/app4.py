c96bc4e10adfdcb9337cc4c80827778c106d2e12
Aroldo <aroldobusiness@hotmail.com>
2023-07-18 16:14:34 -0400

    Python

---
+++ Python/Tkinter/app4/app4.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+def main():
+    menu_inicial = Tk()
+    menu_inicial.title("App4")
+    menu_inicial.geometry("400x400+300+300")
+
+    #Personalizando label
+    label1 = Label(menu_inicial,
+                    text = "Título l",
+                    fg = "black",
+                    font= "Verdana 20")
+    
+    label1.pack()
+
+    label2 = Label(menu_inicial,
+                   text = "Botão",
+                   font = "Verdana 20",
+                   bd = 1,
+                   relief = "solid")
+    label2.pack()
+
+    menu_inicial.mainloop()
+
+main()
