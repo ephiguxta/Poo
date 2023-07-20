4e81f1794c35b707cea9fb84ae61013a060835f4
Aroldo <aroldobusiness@hotmail.com>
2023-07-20 14:00:27 -0400

    Tkinter

--- Python/Tkinter/app7/app7.py
+++ Python/Tkinter/app7/app7.py
@@ -1,27 +1,32 @@
 from tkinter import *
 
-def main():
-    def exibir():
-        l3['text'] = t1.get()
+def executar():
+    l1["text"] = t1.get()
+    l2["text"] = t2.get()
+    l3["text"] = t3.get()
 
-    #GUI
-    root = Tk()
-    root.title("App7")
-    
+root = Tk()
+root.title("App7")
 
-    l1 = Label(root, text="User", padx=10, pady=10, font="Verdana 18").grid(row=0, column=0)
-    l2 = Label(root, text="Senha", padx=10, pady=5, font="Verdana 18").grid(row=1, column=0)
-    t1 = Entry(root)
-    t1.grid()
-    t2 = Entry(root, font="Verdana 18").grid(row=1, column=1)
+t1 = Entry(root)
+t2 = Entry(root)
+t3 = Entry(root)
 
-    l3 = Label(root).grid()
+t1.grid()
+t2.grid()
+t3.grid()
 
-    btn = Button(root, text="Login", font="Verdana 18", bg="gray", command=exibir()).grid(columnspan=2)
+t1.focus()
 
-    root.mainloop()
+l1 = Label(root)
+l2 = Label(root)
+l3 = Label(root)
 
+btn = Button(root, text="Executar", command=executar)
 
+l1.grid()
+l2.grid()
+l3.grid()
+btn.grid()
 
-
-main()
+root.mainloop()
